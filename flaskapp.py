12150292397d622405993fd7ab82a853fada7493
tobinn1 <tnels28@gmail.com>
2016-02-24 17:34:47 +1000

    trying to make it work with openshift

---
+++ flaskapp.py
@@ -0,0 +1,22 @@
+import os, random, string, sys
+from flask import Flask, render_template, send_file
+from PIL import Image, ImageMath, ImageChops, ImageDraw, ImageFont
+from main import Generate
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template('template.html')
+
+@app.route("/temp.jpg")
+def img():
+    return send_file("temp.jpg", mimetype='image/jpg')
+
+@app.route("/generate/")
+def my_link():
+  Generate()
+  return render_template('generate.html')
+
+if __name__ == "__main__":
+  app.run()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Autistic Meme Generator',
+      version='1.0',
+      description='OpenShift App',
+      author='Tobinn1',
+      author_email='tnels28@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+#      install_requires=['Django>=1.3'],
+     )
